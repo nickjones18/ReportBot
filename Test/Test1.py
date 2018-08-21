206f746c2e2643380ba0cb4eadb65a449d0af471
Nicholas Jones <NJones@datacm.com>
2018-08-21 12:24:10 -0400

    Algorithm Update
    Changes made to structure and order of algorithm.

--- Test/Test1.py
+++ Test/Test1.py
@@ -1,14 +1,19 @@
 from selenium import webdriver
 import time
+import getpass
 from selenium.webdriver.support.ui import Select
+from selenium.webdriver.chrome.options import Options
 
 accountOptions = []
 campaignOptions = []
+touchpointOptions = []
 
-driver = webdriver.Chrome(r"C:\Users\njones\PycharmProjects\ReportBot\Drivers\chromedriver.exe")
-# driver.set_page_load_timeout(10)
+options = Options()
+#options.add_argument('--headless')
+#options.add_argument('--disable-gpu')
+driver = webdriver.Chrome(r"C:\Users\njones\PycharmProjects\ReportBot\Drivers\chromedriver.exe", chrome_options=options)
+
 driver.get("http://uweb01/MarketingConsole/Login.aspx?ReturnUrl=%2fMarketingConsole%2fDefault.aspx")
-
 #Get the user's input
 username = input("Username: ")
 password = input("Password: ")
@@ -48,19 +53,19 @@ for opt in opts:
 print("Please choose a campaign from the following list...")
 n = 1
 for i in range (len(campaignOptions)):
-    time.sleep(0.5)
+    time.sleep(0.1)
     print(f"{n}. {campaignOptions[i]}")
     n += 1
     i += 1
 
 campaignNumber = input("Select by number: ")
 
-campaign = str(accountOptions[int(accountNumber)-1])
-
-touchpoint = input("Touchpoint: ")
+campaign = str(campaignOptions[int(campaignNumber)-1])
 
 # Select Campaign
 driver.find_element_by_xpath(f"//select[@id='ctl00_ApplicationContent_MarketingCampaignDropDown']/option[text()='{campaign}']").click()
+
+print("Please wait. This could take a couple minutes.")
 
 # Select Performance
 driver.find_element_by_xpath('//*[@title="Blank Email Performance Report" and text() = "Email Performance"]').click()
@@ -68,8 +73,28 @@ driver.find_element_by_xpath('//*[@title="Blank Email Performance Report" and te
 # Edit
 driver.find_element_by_id("ctl00_ApplicationContent_hlShowDetails").click()
 
-# Edit Touchpoint
+#Prompt for touchpoint
+select = Select(driver.find_element_by_id("ctl00_ApplicationContent_ReportID50_TouchpointDropDown"))
+opts = select.options
+for opt in opts:
+    touchpointOptions.append(opt.text)
+
+print("Please choose a touchpoint from the following list...")
+n = 1
+for i in range (len(touchpointOptions)):
+    time.sleep(0.1)
+    print(f"{n}. {touchpointOptions[i]}")
+    n += 1
+    i += 1
+
+touchpointNumber = input("Select by number: ")
+
+touchpoint = str(touchpointOptions[int(touchpointNumber)-1])
+
+
 driver.find_element_by_xpath(f"//select[@id='ctl00_ApplicationContent_ReportID50_TouchpointDropDown']/option[text()='{touchpoint}']").click()
+
+print("Pulling the reports from the last deployment! They should be in your downloads folder momentarily.")
 
 # Download Performance
 driver.find_element_by_id("ctl00_ApplicationContent_ExportToPNGButton").click()
